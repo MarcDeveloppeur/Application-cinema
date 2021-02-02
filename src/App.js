30dfe966ad9e2a652f69293a98acdb639d45fad9
Marc <marcramadison006@gmail.com>
2021-02-02 16:04:40 +0300

    Ajouter les liens de l'API

--- src/App.js
+++ src/App.js
@@ -3,44 +3,6 @@ import './style/style.css';
 import MovieBox from './components/movieBox';
 
 
-const movies=[
-     {
-       imageUrl:"./assets/Lighthouse.jpg",
-       title:'Premier film',
-       note:4,
-       description:"bla bla bla bla bla ffffffffffff ffffffffffffff fffffffffff ffffffffffffff hhhhhhhhhhh hhhhhhhhhhhhhhhh hhhhhhhhhhhhh hhhhhhhhhh eeeeeeeeeeee eeeeeeeeeee eeeeeeeeeee eeeeeeee eeeeeeeeeeee eeeeeeeeeeeee"
-    },
-    {
-      imageUrl:"./assets/Lighthouse.jpg",
-      title:'le dernier train pour boussan episode 2 partie 1 episode 1 et demi',
-      note:8,
-      description:"bla bla bla bla bla"
-   },
-   {
-     imageUrl:"./assets/Lighthouse.jpg",
-     title:'Premier film',
-     note:4,
-     description:"bla bla bla bla bla ffffffffffff ffffffffffffff fffffffffff ffffffffffffff hhhhhhhhhhh hhhhhhhhhhhhhhhh hhhhhhhhhhhhh hhhhhhhhhh eeeeeeeeeeee eeeeeeeeeee eeeeeeeeeee eeeeeeee eeeeeeeeeeee eeeeeeeeeeeee"
-  },
-  {
-    imageUrl:"./assets/Lighthouse.jpg",
-    title:'Premier film',
-    note:4,
-    description:"bla bla bla bla bla ffffffffffff ffffffffffffff fffffffffff ffffffffffffff hhhhhhhhhhh hhhhhhhhhhhhhhhh hhhhhhhhhhhhh hhhhhhhhhh eeeeeeeeeeee eeeeeeeeeee eeeeeeeeeee eeeeeeee eeeeeeeeeeee eeeeeeeeeeeee"
- },
- {
-   imageUrl:"./assets/Lighthouse.jpg",
-   title:'Premier film',
-   note:4,
-   description:"bla bla bla bla bla ffffffffffff ffffffffffffff fffffffffff ffffffffffffff hhhhhhhhhhh hhhhhhhhhhhhhhhh hhhhhhhhhhhhh hhhhhhhhhh eeeeeeeeeeee eeeeeeeeeee eeeeeeeeeee eeeeeeee eeeeeeeeeeee eeeeeeeeeeeee"
-},
-{
-  imageUrl:"./assets/Lighthouse.jpg",
-  title:'Premier film',
-  note:2,
-  description:"bla bla bla bla bla ffffffffffff ffffffffffffff fffffffffff ffffffffffffff hhhhhhhhhhh hhhhhhhhhhhhhhhh hhhhhhhhhhhhh hhhhhhhhhh eeeeeeeeeeee eeeeeeeeeee eeeeeeeeeee eeeeeeee eeeeeeeeeeee eeeeeeeeeeeee"
-},
-]
 function App() {
   const [search,setSearch]=useState("");
   const [data,setData]=useState([]);
@@ -50,8 +12,10 @@ function App() {
    useEffect(()=>{
      fetch('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1')
      .then((res)=>res.json())
-     .then((data)=>{
-       //setData(data.results);
+     .then((resultat)=>{
+       setData(resultat.results);
+       console.log(resultat.results);
+       console.log(resultat.backdrop_path);
      });
    },[]);
 
@@ -62,7 +26,7 @@ function App() {
     fetch('https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query='+search)
     .then((res)=>res.json())
     .then((data)=>{
-      //setData(result.results);
+      setData(data.results);
     })
   }
   return (
@@ -74,7 +38,7 @@ function App() {
              </form>
         </div>
         <div className="containtBlock">
-            {movies.map((movie,id)=><MovieBox key={id} imageUrl={movie.imageUrl} title={movie.title} note={movie.note} description={movie.description}/>)}
+            {data.map((movie,id)=><MovieBox key={id} pathImage={movie.poster_path} title={movie.original_title} note={movie.vote_average} description={movie.overview}/>)}
         </div>
     </div>
   );
--- src/components/movieBox.js
+++ src/components/movieBox.js
@@ -5,7 +5,7 @@ function MovieBox(props) {
     const APIimage="https://image.tmdb.org/t/p/w1280";
   return (
     <div className="boxContainer">
-          <img src={props.imageUrl} alt="image d'un film" className="imageBox"/>
+          <img src={props.pathImage?(APIimage+props.pathImage):"https://images.unsplash.com/photo-1542204165-65bf26472b9b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80"} alt="image d'un film" className="imageBox"/>
           <div className="about">
                <p className="movieTitle">
                    {props.title}
